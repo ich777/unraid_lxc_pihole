ed7519f206a5d05bcb5a860c9caa618ae2b9b738
Christoph Hummer <christophhummer@gmail.com>
2023-09-13 19:04:57 +0200

    Add files via upload

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get -y install wget curl nano unbound keepalived iputils-ping openssh-server cron netctl
+apt-get -y install wget curl nano unbound keepalived iputils-ping openssh-server cron
 systemctl stop unbound keepalived ssh.service
 systemctl disable keepalived

18ae97339705fded719e1b3ceb8a497728680eb9
Christoph Hummer <christophhummer@gmail.com>
2023-09-17 09:14:53 +0200

    Update 01-dependencies.sh

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -3,3 +3,5 @@ apt-get update
 apt-get -y install wget curl nano unbound keepalived iputils-ping openssh-server cron
 systemctl stop unbound keepalived ssh.service
 systemctl disable keepalived
+sed -i "s/#DNSStubListener=yes/DNSStubListener=no/g" /etc/systemd/resolved.conf && \
+systemctl restart systemd-resolved

f001203c390697b684ad418e4cc3ba5ab9094e5d
Christoph Hummer <christophhummer@gmail.com>
2023-10-19 12:08:34 +0200

    Update 02-pihole.sh

--- build/02-pihole.sh
+++ build/02-pihole.sh
@@ -4,3 +4,4 @@ cp /tmp/build/setupVars.conf /etc/pihole/
 chmod 755 /etc/pihole/setupVars.conf
 export PIHOLE_SKIP_OS_CHECK=true
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
+sed -ie 's/\<80\>/8080/g' /etc/lighttpd/lighttpd.conf
